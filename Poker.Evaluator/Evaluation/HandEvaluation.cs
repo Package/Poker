4b46e2a0daed555410f623e5f52cc0a528389c19
Package <ryan.dean1000@outlook.com>
2020-06-27 00:50:50 +0100

    Hand Strength Evaluator and Unit Tests

---
+++ Poker.Evaluator/Evaluation/HandEvaluation.cs
@@ -0,0 +1,254 @@
+﻿using Poker.Domain;
+using Poker.Domain.Hands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Poker.Evaluator.Evaluation
+{
+    public sealed class HandEvaluation
+    {
+        /// <summary>
+        /// From the hand provided, calculates the strength of it.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        public static HandStrength Strength(Hand hand)
+        {
+            var values = BuildValueMap(hand);
+
+            var flush = GetFlushCards(hand);
+            var straight = GetStraightCards(hand);
+            var pairs = Pairs(values);
+            var trips = OfAKind(values, 3);
+            var quads = OfAKind(values, 4);
+
+            if (quads > 0) 
+                return HandStrength.FourOfAKind;
+
+            if (trips > 0 && pairs > 0) 
+                return HandStrength.FullHouse;
+
+            if (flush != null) 
+                return BestFlush(flush, straight);
+
+            if (straight != null) 
+                return HandStrength.Straight;
+
+            if (trips > 0) 
+                return HandStrength.ThreeOfAKind;
+
+            if (pairs >= 2) 
+                return HandStrength.TwoPair;
+
+            if (pairs == 1) 
+                return HandStrength.Pair;
+
+            return HandStrength.HighCard;
+        }
+
+        /// <summary>
+        /// Returns the number of pairs in the hand.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static int Pairs(Dictionary<Value, int> values) => values.Values.Count(v => v == 2);
+
+        /// <summary>
+        /// Returns how many values of the same kind are in the hand.
+        /// E.g. OfAKind(values, 3) would return how many three of a kinds are in the hand values.
+        /// E.g. OfAKind(values, 4) would return how many four of a kinds are in the hand values.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        private static int OfAKind(Dictionary<Value, int> values, int amount) => values.Values.Count(v => v == amount);
+
+        /// <summary>
+        /// Returns the best flush that is possible. Checks for Royal and Straight flushes.
+        /// </summary>
+        /// <param name="flushCards"></param>
+        /// <param name="straightCards"></param>
+        /// <returns></returns>
+        private static HandStrength BestFlush(IEnumerable<Card> flushCards, IEnumerable<Card> straightCards)
+        {
+            // No straight on the board - impossible to have anything better than a flush
+            if (straightCards == null)
+                return HandStrength.Flush;
+
+            var counter = 0;
+            var flushSuit = flushCards.First().Suit;
+
+            /*
+             * Check for royal flush.
+             * Straight cards in range 10 to Ace need to be in same suit for royal flush
+             */ 
+            var values = (Value[]) Enum.GetValues(typeof(Value));
+            var tenToAce = values[^5..^0]; // Ten to Ace range
+            foreach (var value in tenToAce)
+            {
+                var card = straightCards.FirstOrDefault(c => c.Value == value && c.Suit == flushSuit);
+                if (card != null)
+                    counter += 1;
+            }
+            if (counter == 5)
+            {
+                // Found all five in the straight cards, this is a royal flush
+                return HandStrength.RoyalFlush;
+            }
+
+            /*
+             * Check for straight flush.
+             * Five or more cards in the straight need to be of the same suit for the straight flush
+             */
+            counter = 0;
+            foreach (var card in straightCards)
+            {
+                if (flushCards.Contains(card))
+                {
+                    counter += 1;
+                }
+                else
+                {
+                    // This card isn't, but we've already seen enough to know it's a straight flush
+                    if (counter >= 5)
+                        return HandStrength.StraightFlush;
+                    else
+                        counter = 0;
+                }
+            }
+
+            return counter >= 5 ? HandStrength.StraightFlush : HandStrength.Flush;
+        }
+
+        /// <summary>
+        /// Determines whether the hand is a flush.
+        /// That is, five or more cards are of the same suit.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static bool IsFlush(Dictionary<Suit, int> suits) => suits.Values.Max() >= 5;
+
+        /// <summary>
+        /// Returns all the cards which are making this hand a flush (if any).
+        /// If the hand does not contain a flush, then null is returned.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static IEnumerable<Card> GetFlushCards(Hand hand)
+        {
+            var suitMap = BuildSuitMap(hand);
+            if (!IsFlush(suitMap))
+                return null;
+
+            var flushSuit = suitMap.FirstOrDefault(s => s.Value >= 5).Key;
+            return hand.Cards.Where(c => c.Suit == flushSuit);
+        }
+
+        /// <summary>
+        /// Returns the cards in the provided hand which make a straight.
+        /// Note that this will return all the cards in the straight, even if there is more than 5.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static IEnumerable<Card> GetStraightCards(Hand hand)
+        {
+            var orderedCards = hand.Cards.OrderBy(c => c.Value).ToList();
+            
+            // If there's an ace in the hand, put this to the beginning to count for 
+            // low straight (Ace - 5)
+            var ace = hand.Cards.FirstOrDefault(c => c.Value == Value.Ace);
+            if (ace != null)
+            {
+                orderedCards.Insert(0, ace);
+            }
+
+            var lastCard = orderedCards.FirstOrDefault();
+            var straightCards = new List<Card>();
+            straightCards.Add(lastCard);
+
+            for (var index = 1; index < orderedCards.Count; index++)
+            {
+                var currentCard = orderedCards[index];
+                var valueDifference = lastCard.Value.Difference(currentCard.Value);
+
+                if (valueDifference == 1)
+                {
+                    // Difference of exactly 1 means the cards are one value apart, e.g Five -> Six
+                    straightCards.Add(currentCard);
+                }
+                else if (valueDifference == 0)
+                {
+                    // Difference of exactly 0 means the card is the same as the previously seen card, e.g. Jack -> Jack
+                    // In this case, we don't need to do anything...
+
+                    // Todo: we may need to add this to the set of straight cards anyway to account for straight flush situations.
+                }
+                else
+                {
+                    // Difference of anything else means the card is a lot further up in the chain e.g Seven -> Ten.
+                    // If not already a straight then this needs to reset as these cards cannot make a straight
+                    if (straightCards.Count >= 5)
+                        return straightCards;
+                    else
+                    {
+                        straightCards.Clear();
+                        straightCards.Add(currentCard);
+                    }
+                }
+
+                lastCard = currentCard;
+            }
+
+            return straightCards.Count >= 5 ? straightCards : null;
+        }
+
+        /// <summary>
+        /// Builds a map of the values in the hand. How many times each value appears in the hand.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static Dictionary<Value, int> BuildValueMap(Hand hand)
+        {
+            var valueMap = new Dictionary<Value, int>();
+            foreach (var card in hand.Cards)
+            {
+                if (valueMap.ContainsKey(card.Value))
+                {
+                    valueMap[card.Value] += 1;
+                }
+                else
+                {
+                    valueMap[card.Value] = 1;
+                }
+            }
+
+            return valueMap;
+        }
+
+        /// <summary>
+        /// Builds a map of each suit in the hand. How many times each suit appears in the hand.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        private static Dictionary<Suit, int> BuildSuitMap(Hand hand)
+        {
+            var suitMap = new Dictionary<Suit, int>();
+            foreach (var card in hand.Cards)
+            {
+                if (suitMap.ContainsKey(card.Suit))
+                {
+                    suitMap[card.Suit] += 1;
+                }
+                else
+                {
+                    suitMap[card.Suit] = 1;
+                }
+            }
+
+            return suitMap;
+        }
+    }
+}
---
+++ Poker.Tests/Test_Evaluator.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker.Domain;
+using Poker.Domain.Hands;
+using Poker.Evaluator.Evaluation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker.Tests
+{
+    [TestClass]
+    public class Test_Evaluator
+    {
+        [TestMethod]
+        public void CanEvaluateHighCard()
+        {
+            // High Card
+            var hand = Hand.FromString("2S 5H 8C 9C AS KD");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.HighCard);
+        }
+
+        [TestMethod]
+        public void CanEvaluatePairs()
+        {
+            // Two Pair
+            var hand = Hand.FromString("5H 5S TC TD KH 3D 4D");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.TwoPair);
+
+            // One Pair
+            hand = Hand.FromString("QH TD 3S QC AH 2D 6S");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.Pair);
+
+            // Three Pair (should still return two pairs tho)
+            hand = Hand.FromString("5H 5S TC TD KH 6D KS");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.TwoPair);
+        }
+
+        [TestMethod]
+        public void CanEvaluateStraight()
+        {
+            // Standard straight
+            var hand = Hand.FromString("5H 6D 7D QS 8C 9S AS");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+
+            // 7 card straight
+            hand = Hand.FromString("5H 6D 7D TC 8C 9S 4D");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+
+            // Low straight
+            hand = Hand.FromString("AH 2D 4D 5C TC 9S 3D");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+
+            // Low 6 card straight
+            hand = Hand.FromString("AH 2D 4D 5C 6C 9S 3D");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+
+            // 7 card high straight
+            hand = Hand.FromString("9H TD QD KC JC AS 8D");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+
+            // Nut straight
+            hand = Hand.FromString("JH QD TS AS 6C KS 3D");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Straight, strength);
+        }
+
+        [TestMethod]
+        public void CanEvaluateStraightFlush()
+        {
+            // High straight flush
+            var hand = Hand.FromString("9D TD JD QD 6S KD 4C");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.StraightFlush, strength);
+
+            // Low straight flush
+            hand = Hand.FromString("AS 2S 4S 3S 8S 5S");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.StraightFlush, strength);
+
+            // Mid straight flush
+            hand = Hand.FromString("6H 8H 7H 9H 5H 3S AS");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.StraightFlush, strength);
+
+            // Straight & Flush but not a straight flush - should return just flush
+            hand = Hand.FromString("JH 8H 7H 9H 5H 6S AS");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Flush, strength);
+        }
+
+        [TestMethod]
+        public void CanEvaluateRoyalFlush()
+        {
+            // Royal flush
+            var hand = Hand.FromString("TC JC QC KC AC 6S 3D");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.RoyalFlush, strength);
+
+            // 7 card but still royal flush
+            hand = Hand.FromString("TC JC QC KC AC 9C 8C");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.RoyalFlush, strength);
+
+            // Not quite a royal flush - this should just return flush
+            hand = Hand.FromString("TC JC QC KC AS 6S 3C");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(HandStrength.Flush, strength);
+        }
+
+        [TestMethod]
+        public void CanEvaluateFlush()
+        {
+            // A flush
+            var hand = Hand.FromString("6S 7S KS 2S JS AD");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.Flush);
+
+            // Not a flush
+            hand = Hand.FromString("6C 7S KS 2S JS AD");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreNotEqual(strength, HandStrength.Flush);
+        }
+
+        [TestMethod]
+        public void CanEvaluateFullHouse()
+        {
+            // Full House
+            var hand = Hand.FromString("4H 4S TH 4D KH KD 7H");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.FullHouse);
+        }
+
+        [TestMethod]
+        public void CanEvaluateOfAKind()
+        {
+            // Three of a Kind
+            var hand = Hand.FromString("AD 2S TH 4D AS KD AH");
+            var strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.ThreeOfAKind);
+
+            // Four of a Kind
+            hand = Hand.FromString("JD JS JH 4D AS JD AH");
+            strength = HandEvaluation.Strength(hand);
+            Assert.AreEqual(strength, HandStrength.FourOfAKind);
+        }
+    }
+}
