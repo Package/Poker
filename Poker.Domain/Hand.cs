5a7ca85dbe2c0c19c83a47f2c85b5f4d95d8a815
Package <ryan.dean1000@outlook.com>
2020-06-26 16:49:21 +0100

    Hand domain and unit tests

---
+++ Poker.Domain/Hand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Poker.Domain
+{
+    public class Hand
+    {
+        public ICollection<Card> Cards { get; private set; }
+
+        /// <summary>
+        /// Creates a Hand from the provided string.
+        /// E.g. "AD 5H 3C 4C TS" would return Hand which contains 5 Cards.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Hand FromString(string str)
+        {
+            var cards = str.Split(" ").Select(s => Card.FromString(s)).ToList();
+            if (cards == null || !cards.Any())
+            {
+                throw new ArgumentException($"Unable to create hand from input: {str}");
+            }
+
+            return new Hand { 
+                Cards = cards
+            };
+        }
+
+        /// <summary>
+        /// Adds a Card into this Hand.
+        /// </summary>
+        /// <param name="card"></param>
+        public void AddCard(Card card)
+        {
+            if (Cards == null)
+            {
+                Cards = new List<Card>();
+            }
+
+            Cards.Add(card);
+        }
+
+        /// <summary>
+        /// Resets the cards in this hand.
+        /// </summary>
+        public void Reset()
+        {
+            Cards?.Clear();
+        }
+    }
+}
---
+++ Poker.Tests/Test_Hand.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker.Domain;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Poker.Tests
+{
+    [TestClass]
+    public class Test_Hand
+    {
+        [TestMethod]
+        public void CanAddCardToHand()
+        {
+            var h = new Hand();
+            var c1 = Card.FromString("5D");
+            h.AddCard(c1);
+
+            Assert.AreEqual(h.Cards.Count, 1);
+        }
+
+        [TestMethod]
+        public void CanResetHand()
+        {
+            var h = new Hand();
+
+            var c1 = Card.FromString("5D");
+            h.AddCard(c1);
+            Assert.AreEqual(h.Cards.Count, 1);
+
+            h.Reset();
+            Assert.AreEqual(h.Cards.Count, 0);
+        }
+
+        [TestMethod]
+        public void CanBuildHandFromString()
+        {
+            var h1 = Hand.FromString("AD 5H 3C 4C TS");
+            Assert.IsNotNull(h1.Cards);
+            Assert.AreEqual(h1.Cards.Count, 5);
+
+            var c1 = h1.Cards.FirstOrDefault();
+            Assert.AreEqual(c1.Suit, Suit.Diamonds);
+            Assert.AreEqual(c1.Value, Value.AceHigh);
+
+            var h2 = Hand.FromString("AD 5H");
+            Assert.IsNotNull(h2.Cards);
+            Assert.AreEqual(h2.Cards.Count, 2);
+
+            var h3 = Hand.FromString("AD");
+            Assert.IsNotNull(h3.Cards);
+            Assert.AreEqual(h3.Cards.Count, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThrowsErrorWithInvalidString()
+        {
+            var h1 = Hand.FromString("AD5H");
+        }
+    }
+}
